3a43aec0ef3a60ac7485eaf6b65ee335d4bb6236
Vegah92 <mondivegah83@gmail.com>
2023-02-16 09:12:02 -0800

    4-puts.c3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-/*printing a text in c */
+/**
+* main - Entry point
+*
+* Description: 'program prints a text'
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
+/*printing a text in c */
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }

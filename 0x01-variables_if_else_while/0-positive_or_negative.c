df1c036680e8f84598d1eb0f1f15c555e31e56a2
Vegah92 <mondivegah83@gmail.com>
2023-02-17 14:39:46 -0800

    fin0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
+/**
+ * main - Entry point
+ * Description:This program will assign a random number to n
+ * Return:Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {

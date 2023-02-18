606eb114e7c1f6f9c41b62a5944f158141d77a02
Vegah92 <mondivegah83@gmail.com>
2023-02-17 17:35:33 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description:programs print z - a
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int first = 'z';
+while (first <= 'a')
+{
+putchar(first);
+first--;
+}
+putchar('\n');
+return (0);
+}

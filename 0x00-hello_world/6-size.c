ea67dc1aa1fde84e4aacb5fa5eb655f5d656d9a5
Vegah92 <mondivegah83@gmail.com>
2023-02-20 06:14:19 -0800

    correction of Size of a long long int

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ float d;
 printf("Size of a char: %zu byte(s)\n", sizeof(a));
 printf("size of an int: %zu bytes(s)\n", sizeof(b));
 printf("size of a long int: %zu bytes(s)\n", sizeof(c));
-printf("size of a long int: %zu bytes(s)\n", sizeof(e));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(e));
 printf("size of a float: %zu bytes(s)\n", sizeof(d));
 
 

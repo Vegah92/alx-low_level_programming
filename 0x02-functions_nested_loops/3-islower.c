519e8df2c72522190c26e84872690cce2bcdb467
Vegah92 <mondivegah83@gmail.com>
2023-02-21 16:18:10 -0800

    bollean outpu for lower or upper case c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,11 @@
 #include "main.h"
 /**
  *_islower - Entry point
- *Description:checks for lowercase character
- *Return 0 or 1 as responce
+ * @c: this is the letter to be tested
+ *Description:checks if  c is lowercase or uppercase  character
+ *Return:0 or 1
  */
 int _islower(int c)
 {
-
-return (c >= 97 && c <=122);
+return (c >= 97 && c <= 122);
 }

7eda375f05faff43be3b38e500581383ce4b320f
Vegah92 <mondivegah83@gmail.com>
2023-02-21 18:12:29 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *add - Entry point
+ *@c : first input
+ *@d : second input
+ *Description:Adding two numbers
+ *Return: 0
+ */
+int add(int c, int d)
+{
+	return (c + d);
+
+
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,8 @@
+ #include <stdio.h>
+#include "main.h"
+/**
+ *jack_bauer - Entry point
+ *Description: function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ *Return:Always 0 (Success)
+ */
+void jack_bauer(void)

adb8a1faf25aea67cb08464bf9ec7f352593945f
Vegah92 <mondivegah83@gmail.com>
2023-02-21 16:01:05 -0800

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_islower - Entry point
+ *Description:checks for lowercase character
+ *Return 0 or 1 as responce
+ */
+int _islower(int c)
+{
+
+return (c >= 97 && c <=122);
+}

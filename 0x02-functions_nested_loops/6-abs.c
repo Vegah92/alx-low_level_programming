2f2127797e6c1cf40b7a1cb102d8ef1db5a55daa
Vegah92 <mondivegah83@gmail.com>
2023-02-21 17:21:30 -0800

    getting the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *_abs - Entry point
+ *@c : interger to generate absolute value
+ *Description:Returns the absolute value of a number
+ *Return: 0 or 1 (Success)
+ */
+int _abs(int c)
+{
+	return (c >= 0 ? c : c c * -1);
+
+}

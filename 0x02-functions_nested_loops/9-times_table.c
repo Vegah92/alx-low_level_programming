e9920710592f69ee0750157cf4b2cd952f635aae
Vegah92 <mondivegah83@gmail.com>
2023-02-22 16:29:57 -0800

    creating a 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,3 +6,23 @@
  *Return:Always 0 (Success)
  */
 void times_table(void)
+{
+	int n = 9;
+	int i, j;
+	int result;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			result = i * j;
+			_putchar(result);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+
+
+
+}

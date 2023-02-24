5f8572687568c73c3c47984eeb139aa65d351f2f
Vegah92 <mondivegah83@gmail.com>
2023-02-24 14:26:39 -0800

    correction 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,20 +3,20 @@
  *print_line - Entry point
  *@n: number of times line is printed
  *Description:A function thats  prints a line
- *Return:Always 0 
+ *Return:Always 0
  */
 
 void print_line(int n)
 {
 	int a;
 
-	if (n <=0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (a = 0;a < n; a++)
+		for (a = 0; a < n; a++)
 		{
 			_putchar(95);
 		}

742a306a27eac5d33bb2ccc7d41fc4f1933cae6c
Vegah92 <mondivegah83@gmail.com>
2023-02-24 12:07:22 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_isupper - Entry point
+ *@i : lettert to be tested.
+ *Description:A function thats  prints 1 if a letter is uppercase
+ *Return: 1 (Success)
+ */
+
+int _isupper(int i)
+{
+	int i;
+
+	if (i >= 65 && i <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}

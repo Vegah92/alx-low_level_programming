65bfcbab317a80ef582b732dc0a1a940b54e9921
Vegah92 <mondivegah83@gmail.com>
2023-02-24 13:45:24 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *more_numbers - Entry point
+ *Description:A function that prints from 0 to 14
+ *Return:Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; x < 10; x++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			{
+				_putchar((b / 10) + '0');
+						}
+						_putchar((b % 10) + '0');
+						}
+					_putchar('\n');
+}
+}
+

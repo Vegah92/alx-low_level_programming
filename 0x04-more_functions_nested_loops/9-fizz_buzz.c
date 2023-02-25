90a8642e0ce965a922867efbe3cdd57ec7f4275f
Vegah92 <mondivegah83@gmail.com>
2023-02-25 14:27:03 -0800

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,33 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10 - Entry point
- *Description:A function thats  prints the alphabet in small letters 10 times
+ *main - Entry point
+ *Description:print FizzBuzz
  *Return:Always 0 (Success)
  */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	return (0);
+}

3d63284205ec5f8e0c948a5e447902de0f536508
Vegah92 <mondivegah83@gmail.com>
2023-02-27 12:46:28 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,2 +1,14 @@
 #include "main.h"
 
+
+/**
+ *reset_to_98 - Entry point
+ *@n : pointer
+ *Description:A function thats  that takes a pointer as a parameter
+ *Returns: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

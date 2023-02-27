b6c810b54497c932bc9d4b93ea06a0241497642e
Vegah92 <mondivegah83@gmail.com>
2023-02-27 13:00:58 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *swap_int - Entry point
+ *@a : pointers
+ *@b : pointers 2
+ *Description:A function thats  swap intergers, using pointers as parameters
+ *Return:Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	int k;
+
+	*a = k;
+	*a = *b;
+	*b = k`;
+}
+

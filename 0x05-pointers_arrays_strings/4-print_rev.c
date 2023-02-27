fd40ec85f42f483081a3d597a1ec17852a51a010
Vegah92 <mondivegah83@gmail.com>
2023-02-27 13:54:14 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_rev - Entry point
+ *@s : string
+ *Description:A function thats  prints a string in reverse order
+ *Return:Always 0
+ */
+
+void print_rev(char *s)
+{
+	int length;
+	int m;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	for (m = length; m > 0; m--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
+

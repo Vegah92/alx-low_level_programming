be3fb3334a754ddcab074916d9ee458a6e063614
Vegah92 <mondivegah83@gmail.com>
2023-03-06 07:20:22 -0800

    5corret

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *_strstr - Entry point
+ *@haystac: function parameter
+ *@needle: function parameters
+ *Description:A function that locates a substring
+ *Return:Always 0 (Success)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *a = haystack;
+		char *b = needle;
+
+		while (*a == *b && *b != '\0')
+		{
+			a++;
+			b++;
+		}
+
+		if (*b == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
+

74b7b6e7e94f44e93243905c36f2d57a23c40ab4
Vegah92 <mondivegah83@gmail.com>
2023-03-13 17:18:23 -0700

    0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * @argc:function parameters
+ * @argv:function vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+		printf("%s\n", argv[i]);
+	return (0);
+}

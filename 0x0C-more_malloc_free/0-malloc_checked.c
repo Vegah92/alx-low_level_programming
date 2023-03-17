76ce59db871b24da5cf1179e04bc3ac5fdb60338
Vegah92 <mondivegah83@gmail.com>
2023-03-16 17:25:02 -0700

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+*malloc_checked - entry point
+*@b:function parameter
+*Description: function that allocates memory using malloc
+*Return: pointer to location
+*/
+
+
+void *malloc_checked(unsigned int b)
+{
+void *a;
+
+a = malloc(unsignes int b)
+if (a == NULL)
+{
+exit(98);
+}
+return (a);
+}

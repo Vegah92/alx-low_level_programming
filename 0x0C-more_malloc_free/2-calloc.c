437f999238fb76d1538bbcac6c1d7ef425774f46
Vegah92 <mondivegah83@gmail.com>
2023-03-16 19:14:18 -0700

    3-array_range

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*_calloc - entyr point
+*@nmemb: function parameter
+*@size:memory size
+*Description:function that allocates memory for an array, using
+*Return:0
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int a;
+		
+
+		
+	for (a = 0; a < size; a++)
+		
+	{
+		
+		s[i] = b;
+		
+	}
+		
+
+		
+	return (s);
+		
+}
+
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+*array_range - entyr point
+*@min:array parameters
+*@max:function parameters
+*Description:function that creates an array of integers
+*Return:Array
+*/
+int *array_range(int min, int max);
+{
+int *tr;
+int a, size;
+
+if (min > max)
+return (NULL);
+
+size = max - min + 1;
+
+tr = malloc(sizeof(int) * size);
+
+if (tr == NULL)
+return (NULL);
+
+for (a = 0; min <= max; a++)
+
+tr[i] = min++;
+
+return (tr);
+}

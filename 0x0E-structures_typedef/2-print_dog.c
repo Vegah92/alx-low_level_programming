406af708a1d1b5bec494991f4771000b0248ca59
Vegah92 <mondivegah83@gmail.com>
2023-03-21 11:22:10 -0700

    correction 2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
-#include<stdlib>
+#include<stdlib.h>
+#include<stdio.h>
 
 /**
 *print_dog - entry point
@@ -15,9 +16,9 @@ return;
 }
 
 if (d->name == NULL)
-d->name = "nil";
+d->name = "(nil)";
 if (d->owner == NULL)
-d->owner = "nil";
+d->owner = "(nil)";
 
 printf("Name:%s\n Age:%f\n Owner:%s\n", d->name, d->age, d->owner);
 }

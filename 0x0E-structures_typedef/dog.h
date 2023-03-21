233a1b21462135a5965a9e8b9aa90d2807dfa6ec
Vegah92 <mondivegah83@gmail.com>
2023-03-21 07:01:58 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - entry
+*@name:member of structure
+*@owner:member
+*@age:structure member
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif

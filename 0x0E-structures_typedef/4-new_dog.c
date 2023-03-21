2bdf3c10f7aee6b2389fe65a34dda4e63e9dbb25
Vegah92 <mondivegah83@gmail.com>
2023-03-21 11:56:57 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+*new_dog - entry point
+*@name:parameter
+*@age:struct member
+*@owner:member of structure
+*Description:function that creates a new dog
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
